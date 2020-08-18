7e26b8b14ea367c319fb501ea01355417031691b
thomaspetty <thomaskpetty@gmail.com>
2020-08-18 14:10:45 -0500

    Initial Commit
    -Sping Initializer
    -Dev tools, Web, and Thymeleaf

---
+++ src/test/java/org/launchcode/Atlas/AtlasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Atlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
