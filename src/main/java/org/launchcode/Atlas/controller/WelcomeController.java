e46f16db817878e2fa10141ae1d8fb4659867529
thomaspetty <thomaskpetty@gmail.com>
2020-08-18 14:20:22 -0500

    Establish mapping to 'Hello world' test page
    -confirms initial set up

---
+++ src/main/java/org/launchcode/Atlas/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package org.launchcode.Atlas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping
+    public static String hello(){
+        return "hello-world";
+    }
+}
